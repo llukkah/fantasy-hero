bb0564b717248131afed3b3f12cbc45147aaeeab
llukkah <llukkah.rey@gmail.com>
2020-04-16 12:47:49 -0400

    sign in page needs merge

--- client/src/components/HeroEdit.jsx
+++ client/src/components/HeroEdit.jsx
@@ -5,131 +5,124 @@ import Layout from './shared/Layout'
 import { getHero, updateHero } from '../services/hero'
 
 class HeroEdit extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hero: {
-        _id: '',
-        name: '',
-        specialty: {}, //Come back to this
-        race: '',
-        hp: 100,
-        atk: 50,
-        weapon: '',
-        img: '',
-        description: ''
-      },
-      updated: false
+    constructor(props) {
+        super(props)
+        this.state = {
+          hero: {
+            _id: '',
+            name: '',
+            specialty: {}, //Come back to this
+            race: '',
+            hp: 100,
+            atk: 50,
+            weapon: '',
+            img: '',
+            description: ''
+        },
+            updated: false
+        }
     }
-  }
 
-
-  async componentDidMount() {
-    let { id } = this.props.match.params
-    const hero = await getHero(id)
-    this.setState({ hero })
-    // console.log(this.state.hero)
-    // console.log(this.state.hero._id)
-  }
-
-
-  handleChange = (event) => {
-    const { name, value } = event.target
-    this.setState({
-      hero: {
-        ...this.state.hero,
-        [name]: value //come back to this
-      }
-    })
-  }
-
-  handleSubmit = async (event) => {
-    event.preventDefault()
-    let { id } = this.props.match.params
-    const updated = await updateHero(id, this.state.hero)
-    this.setState({ updated })
-  }
-
-  render() {
-
-    const { hero, updated } = this.state
-    if (updated) {
-      return <Redirect to={`/heroes`} />
+    async componentDidMount() {
+        let { id } = this.props.match.params
+        const hero = await getHero(id)
+        this.setState({ hero })
+        // console.log(this.state.hero)
+        // console.log(this.state.hero._id)
     }
 
 
-    return (
-      <Layout user={this.props.user}>
-        <div className="product-edit">
-          <div className="image-container">
-            {/* <img className="edit-product-image" src={hero.img} alt={hero.name} /> */}
-            <form onSubmit={this.handleSubmit}>
-            </form>
-          </div>
-          {/* <div className="stackDiv"> */}
-          <form className="edit-form" onSubmit={this.handleSubmit}>
-            <input
-              className="input-name"
-              placeholder='Name'
-              value={hero.name}
-              name='name'
-              required
-              autoFocus
-              onChange={this.handleChange}
-            />
-            <input
-              className="input-price"
-              placeholder='Enter a description'
-              value={hero.description}
-              name='description'
-              required
-              onChange={this.handleChange}
-            />
+    handleChange = (event) => {
+        const { name, value } = event.target
+        this.setState({
+            hero: {
+                ...this.state.hero,
+                [name]: value //come back to this
+            }
+        })
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        let { id } = this.props.match.params
+        const updated = await updateHero(id, this.state.hero)
+        this.setState({ updated })
+        // console.log(this.state.updated)
+    }
+
+    render() {
+
+        const { hero, updated } = this.state
+
+// =========COME BACK TO THIS========================
+        if (updated) {
+            return <Redirect to={`/heroes`} />
+            // return <Redirect to={{
+            //     pathname: `/heroes/${this.state.hero._id}`,
+            //     state: { hero: updated }
+            // }} />
+        }
+// =========COME BACK TO THIS========================
 
 
-            <select
-              name="race"
-              onChange={this.handleChange}
-              className="race-select">
-              <option>Select Race</option>
-              <option value='human'>Human</option>
-              <option value='orc'>orc</option>
-              <option value='elf'>Elf</option>
-              <option value='dwarf'>Dwarf</option>
-              <option value='goblin'>Goblin</option>
-              <option value='troll'>Troll</option>
-            </select>
-            {/* <input
-              className="input-price"
-              placeholder='Race'
-              value={hero.race} // come back to this
-              name='race'
-              required
-              onChange={this.handleChange}
-            /> */}
-            <input
-              className="input-price"
-              placeholder='Weapon'
-              value={hero.weapon} // come back to this
-              name='weapon'
-              required
-              onChange={this.handleChange}
-            />
-            <input
-              className="edit-input-image-link"
-              placeholder='Image Link'
-              value={hero.img}
-              name='img'
-              required
-              onChange={this.handleChange}
-            />
-            <button type='submit' className="save-button">Save</button>
-          </form>
-          {/* </div> */}
-        </div>
-      </Layout>
-    )
-  }
+        return (
+            <Layout user={this.props.user}>
+                <div className="product-edit">
+                    <div className="image-container">
+                        {/* <img className="edit-product-image" src={hero.img} alt={hero.name} /> */}
+                        <form onSubmit={this.handleSubmit}>
+                        </form>
+                    </div>
+                    {/* <div className="stackDiv"> */}
+                    <form className="edit-form" onSubmit={this.handleSubmit}>
+                        <input
+                            className="input-name"
+                            placeholder='Name'
+                            value={hero.name}
+                            name='name'
+                            required
+                            autoFocus
+                            onChange={this.handleChange}
+                        />
+                        <input
+                        className="input-price"
+                        placeholder='Enter a description'
+                        value={hero.description}
+                        name='description'
+                        required
+                        onChange={this.handleChange}
+                        />
+                        <input
+                            className="input-price"
+                            placeholder='Race'
+                            value={hero.race} // come back to this
+                            name='race'
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="input-price"
+                            placeholder='Weapon'
+                            value={hero.weapon} // come back to this
+                            name='weapon'
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="edit-input-image-link"
+                            placeholder='Image Link'
+                            value={hero.img}
+                            name='img'
+                            required
+                            onChange={this.handleChange}
+                        />
+                        <button type='submit' className="save-button">Save</button>
+                    </form>
+                    {/* </div> */}
+                </div>
+            </Layout>
+        )
+    }
 }
 
 export default HeroEdit
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -4,58 +4,43 @@ import './SignIn.css'
 import { signIn } from '../services/user'
 
 class SignIn extends Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.state = {
-      username: '',
-      password: '',
-      isError: false,
-      errorMsg: ''
+        this.state = {
+            username: '',
+            password: '',
+            isError: false,
+            errorMsg: ''
+        }
     }
-  }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-      isError: false,
-      errorMsg: ''
-    })
-  }
-
-  onSignIn = event => {
-    event.preventDefault()
-
-    const { history, setUser } = this.props
-
-    signIn(this.state)
-      .then(res => {
-        setUser(res.user)
-      })
-      .then(() => history.push('/'))
-      .catch(error => {
-        console.error(error)
+    handleChange = event => {
         this.setState({
-          isError: true,
-          errorMsg: 'Invalid Credentials',
-          username: '',
-          password: ''
+            [event.target.name]: event.target.value,
+            isError: false,
+            errorMsg: ''
         })
-      })
-  }
+    }
 
-  renderError = () => {
-    const toggleForm = this.state.isError ? 'danger' : '';
-    return this.state.isError ? <button type="submit" className={toggleForm}>
-      {this.state.errorMsg}</button> : <button type="submit">Sign In</button>
+    onSignIn = event => {
+        event.preventDefault()
 
+        const { history, setUser } = this.props
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of cf40bdd... Merge pull request #34 from llukkah/jason3
         signIn(this.state)
             .then(res => {
                 setUser(res.user)
             })
+<<<<<<< HEAD
             .then(() => history.push('/home'))
+=======
+            .then(() => history.push('/'))
+>>>>>>> parent of cf40bdd... Merge pull request #34 from llukkah/jason3
             .catch(error => {
                 console.error(error)
                 this.setState({
@@ -79,39 +64,45 @@ class SignIn extends Component {
             return <button type="submit">Sign In</button>
         }
     }
+<<<<<<< HEAD
   }
 
 
   render() {
     const { username, password } = this.state
+=======
 
-    return (
-      <div className="form-container">
-        <h3>Sign In</h3>
-        <form onSubmit={this.onSignIn}>
-          <label>Username</label>
-          <input
-            required
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Enter Username"
-            onChange={this.handleChange}
-          />
-          <label>Password</label>
-          <input
-            required
-            name="password"
-            value={password}
-            type="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-          />
-          {this.renderError()}
-        </form>
-      </div>
-    )
-  }
+    render() {
+        const { username, password } = this.state
+>>>>>>> parent of cf40bdd... Merge pull request #34 from llukkah/jason3
+
+        return (
+            <div className="form-container">
+                <h3>Sign In</h3>
+                <form onSubmit={this.onSignIn}>
+                    <label>Username</label>
+                    <input
+                        required
+                        type="text"
+                        name="username"
+                        value={username}
+                        placeholder="Enter Username"
+                        onChange={this.handleChange}
+                    />
+                    <label>Password</label>
+                    <input
+                        required
+                        name="password"
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                    />
+                    {this.renderError()}
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SignIn
