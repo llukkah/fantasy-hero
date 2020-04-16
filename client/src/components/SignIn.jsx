1c356befeebd29899381cb45ab55712a8cb4349f
Jackson Vu <jacksonvu@Jacksons-MBP.home>
2020-04-16 12:45:47 -0400

    fixed bad merge

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -4,52 +4,29 @@ import './SignIn.css'
 import { signIn } from '../services/user'
 
 class SignIn extends Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.state = {
-      username: '',
-      password: '',
-      isError: false,
-      errorMsg: ''
+        this.state = {
+            username: '',
+            password: '',
+            isError: false,
+            errorMsg: ''
+        }
     }
-  }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-      isError: false,
-      errorMsg: ''
-    })
-  }
-
-  onSignIn = event => {
-    event.preventDefault()
-
-    const { history, setUser } = this.props
-
-    signIn(this.state)
-      .then(res => {
-        setUser(res.user)
-      })
-      .then(() => history.push('/'))
-      .catch(error => {
-        console.error(error)
+    handleChange = event => {
         this.setState({
-          isError: true,
-          errorMsg: 'Invalid Credentials',
-          username: '',
-          password: ''
+            [event.target.name]: event.target.value,
+            isError: false,
+            errorMsg: ''
         })
-      })
-  }
+    }
 
-  renderError = () => {
-    const toggleForm = this.state.isError ? 'danger' : '';
-    return this.state.isError ? <button type="submit" className={toggleForm}>
-      {this.state.errorMsg}</button> : <button type="submit">Sign In</button>
+    onSignIn = event => {
+        event.preventDefault()
 
-
+        const { history, setUser } = this.props
 
         signIn(this.state)
             .then(res => {
@@ -79,39 +56,37 @@ class SignIn extends Component {
             return <button type="submit">Sign In</button>
         }
     }
-  }
 
+    render() {
+        const { username, password } = this.state
 
-  render() {
-    const { username, password } = this.state
-
-    return (
-      <div className="form-container">
-        <h3>Sign In</h3>
-        <form onSubmit={this.onSignIn}>
-          <label>Username</label>
-          <input
-            required
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Enter Username"
-            onChange={this.handleChange}
-          />
-          <label>Password</label>
-          <input
-            required
-            name="password"
-            value={password}
-            type="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-          />
-          {this.renderError()}
-        </form>
-      </div>
-    )
-  }
+        return (
+            <div className="form-container">
+                <h3>Sign In</h3>
+                <form onSubmit={this.onSignIn}>
+                    <label>Username</label>
+                    <input
+                        required
+                        type="text"
+                        name="username"
+                        value={username}
+                        placeholder="Enter Username"
+                        onChange={this.handleChange}
+                    />
+                    <label>Password</label>
+                    <input
+                        required
+                        name="password"
+                        value={password}
+                        type="password"
+                        placeholder="Password"
+                        onChange={this.handleChange}
+                    />
+                    {this.renderError()}
+                </form>
+            </div>
+        )
+    }
 }
 
 export default SignIn
